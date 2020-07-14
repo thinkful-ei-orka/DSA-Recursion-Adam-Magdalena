4a42148d55a52063833d798798269dcd8ac7927d
Adam Brown <adam.edward.brown@gmail.com>
2020-07-14 14:36:23 -0400

    assignments completed

--- Checkpoint-3.js
+++ Checkpoint-3.js
@@ -69,3 +69,22 @@
 // };
 
 // console.log(factorial(6));
+
+function towerOfHanoi(
+  depth,
+  source = 0,
+  dest = 2,
+  temp = 1,
+  status = [Array.from(Array(depth), (_, i) => depth - i), [], []]) {
+  if (depth === 1) {
+    console.log(`Moved ${source} to ${dest}`);
+    status[dest].push(status[source].pop());
+    console.log(status);
+    return
+  }
+  towerOfHanoi(depth-1, source, temp, dest, status);
+  towerOfHanoi(1, source, dest, temp, status);
+  towerOfHanoi(depth-1, temp, dest, source, status);
+}
+
+towerOfHanoi(3);
